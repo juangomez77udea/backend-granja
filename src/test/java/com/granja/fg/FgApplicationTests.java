51f81b58d86ce230153f03c12d7a4b7539134f7a
juan <juan.gomez77@udea.edu.co>
2024-09-09 20:56:47 -0500

    Commit inicial, se inicia el repositorio

---
+++ src/test/java/com/granja/fg/FgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.granja.fg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
